8496105a765e368b7c2686c9f989a076e8b149b3
xSalmoon <114058917+xSalmoon@users.noreply.github.com>
2023-01-29 15:47:58 +0800

    Add files via upload

---
+++ Final/src/Code/Snake.java
@@ -0,0 +1,44 @@
+package Code;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+       
+/**
+ *
+ * @author pushg
+ */
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Snake extends JFrame {
+
+    public Snake() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        add(new Board());
+               
+        setResizable(false);
+        pack();
+        
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Snake();
+            ex.setVisible(true);
+        });
+    }
+}
